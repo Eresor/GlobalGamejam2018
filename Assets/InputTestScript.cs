59b5d3f6a265c96eb0f774a7fc0c415ee0783f70
Andrzej Okenczyc <bakandrev@gmail.com>
2018-01-26 22:42:12 +0100

    Input manager abstraction for 4 players.

---
+++ Assets/InputTestScript.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Managers;
+using UnityEngine;
+
+public class InputTestScript : MonoBehaviour {
+
+    #region MonoBehaviour Callbacks
+
+    // Update is called once per frame
+    void Update ()
+	{
+
+	    var playersControllers = Enum.GetValues(typeof(InputManager.Player));
+
+	    var axises = Enum.GetValues(typeof(InputManager.Axis));
+
+        var buttons = Enum.GetValues(typeof(InputManager.Buttons));
+
+        foreach (InputManager.Player player in playersControllers)
+	    {
+	        foreach (InputManager.Axis axis in axises)
+	        {
+	            var value = InputManager.GetPlayerAxis(player, axis);
+	            if (value > 0.2 || value < -0.2)
+	            {
+	                Debug.Log(string.Format("Player {0}, Axis: {1}, Value: {2}", player, axis, value));
+	            }          
+            }
+
+	        foreach (InputManager.Buttons button in buttons)
+	        {
+	            if (InputManager.GetPlayerButton(player, button))
+	            {
+	                Debug.Log(string.Format("Player {0}, Button {1}", player, button));
+                }
+	        }
+        }
+	}
+
+    #endregion
+}
---
+++ Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+namespace Managers
+{
+    public static class InputManager
+    {
+        #region Enums
+
+        public enum Axis
+        {
+            Horizontal,
+            Vertical
+        }
+
+        public enum Buttons
+        {
+            A,
+            B,
+            X,
+            Y
+        }
+
+        public enum Player
+        {
+            P1,
+            P2,
+            P3,
+            P4
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public static float GetPlayerAxis(Player playerNumber, Axis axis)
+        {
+            return Input.GetAxis(string.Format("{0}{1}", axis, playerNumber));
+        }
+
+        public static bool GetPlayerButton(Player playerNumber, Buttons button)
+        {
+            return Input.GetButton(string.Format("{0}{1}", button, playerNumber));
+        }
+
+        public static bool GetPlayerButtonUp(Player playerNumber, Buttons button)
+        {
+            return Input.GetButtonUp(string.Format("{0}{1}", button, playerNumber));
+        }
+
+        public static bool GetPlayerButtonDown(Player playerNumber, Buttons button)
+        {
+            return Input.GetButtonDown(string.Format("{0}{1}", button, playerNumber));
+        }
+
+        #endregion
+    }
+}
