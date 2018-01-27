1bf7cc9af72288aebd71ba2745f5753e4ef60ad0
Andrzej Okenczyc <bakandrev@gmail.com>
2018-01-27 20:25:28 +0100

    Picking wood almost done

---
+++ Assets/Wood.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using ProgressBar;
+using UnityEngine;
+
+public class Wood : MonoBehaviour
+{
+    public float progress = 0.00f;
+
+    public GameObject progressBar;
+    public GameObject visibleObject;
+    public GameObject woodIcon;
+    public GameObject axeIcon;
+
+    public GameObject spawnedWood = null;
+
+	// Update is called once per frame
+	void Update () {
+
+	    if (progress == 0.00f && spawnedWood == null)
+	    {
+	        visibleObject.SetActive(false);
+	        axeIcon.SetActive(true);
+	        woodIcon.SetActive(false);
+	    }
+	    else if(progress >= 100f)
+	    {
+	        progressBar.GetComponent<ProgressRadialBehaviour>().Value = 0;
+            visibleObject.SetActive(false);
+	        axeIcon.SetActive(false);
+	        woodIcon.SetActive(true);
+        }
+        else 
+	    {
+	        visibleObject.SetActive(true);
+	        axeIcon.SetActive(false);
+	        woodIcon.SetActive(false);
+        }
+
+	    progressBar.GetComponent<ProgressRadialBehaviour>().Value = progress;
+
+	}
+}
