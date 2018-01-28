7fca996eecfb35cb9ece94fc0cc4377012a787b6
PiotrMuch <piotr.muchlinski@gmail.com>
2018-01-28 02:43:45 +0100

    skeleton movement sound

--- Assets/Scripts/EnemyEventScript.cs
+++ Assets/Scripts/EnemyEventScript.cs
@@ -4,18 +4,28 @@ using UnityEngine;
 
 public class EnemyEventScript : MonoBehaviour {
 
+    public AudioClip clipStep;
+    public AudioClip clipHit;
+    private AudioSource audioSource;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        audioSource = GetComponent<AudioSource>();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
 	}
+
+    public void FootStep()
+    {
+        audioSource.PlayOneShot(clipStep);
+    }
 
     public void damageGate()
     {
         gateScript.hitGate();
+        audioSource.PlayOneShot(clipHit);
     }
 }
