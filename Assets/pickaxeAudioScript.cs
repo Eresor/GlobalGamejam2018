ce0c314d29ae12d69e335d9fb6fef5abfcdbe704
PiotrMuch <piotr.muchlinski@gmail.com>
2018-01-28 05:14:21 +0100

    update

---
+++ Assets/pickaxeAudioScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickaxeAudioScript : MonoBehaviour {
+    
+    private AudioSource audioSource;
+    public AudioClip clip;
+
+    // Use this for initialization
+    void Start () {
+        audioSource = GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void pickaxeHit () {
+        audioSource.PlayOneShot(clip);
+    }
+}
