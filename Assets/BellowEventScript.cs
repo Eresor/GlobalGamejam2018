d11eed75a3a0198ceed600b542505ef152bb10f9
PiotrMuch <piotr.muchlinski@gmail.com>
2018-01-28 13:14:08 +0100

    bellow sounds

---
+++ Assets/BellowEventScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BellowEventScript : MonoBehaviour {
+
+
+    private AudioSource audioSource;
+    public AudioClip pumpDownClip, pumpUpClip;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    void PumpDown()
+    {
+        audioSource.PlayOneShot(pumpDownClip);
+    }
+    void PumpUp()
+    {
+        audioSource.PlayOneShot(pumpUpClip);
+    }
+}
